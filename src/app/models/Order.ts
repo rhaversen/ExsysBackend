83229dff5b0054784ddb28effa1005d39241c621
rhaversen <rhaversen@gmail.com>
2024-04-19 01:35:03 +0200

    Add validations

--- src/app/models/Order.ts
+++ src/app/models/Order.ts
@@ -5,6 +5,9 @@ import { type Document, model, Schema, type Types } from 'mongoose'
 
 // Own modules
 import logger from '../utils/logger.js'
+import OptionModel from './Option.js'
+import ProductModel from './Product.js'
+import RoomModel from './Room.js'
 
 // Destructuring and global variables
 
@@ -27,22 +30,45 @@ export interface IOrder extends Document {
 const orderSchema = new Schema<IOrder>({
     requestedDeliveryDate: {
         type: Schema.Types.Date,
-        required: [true, 'Requested delivery date is required']
+        required: [true, 'Requested delivery date is required'],
+        validate: {
+            validator: function (v: Date) {
+                // Ensure the requested delivery date is not in the past
+                return v >= new Date()
+            },
+            message: 'Requested delivery date cannot be in the past'
+        }
     },
     roomId: {
         type: Schema.Types.ObjectId,
         ref: 'Room',
-        required: [true, 'Room name is required']
+        required: [true, 'Room name is required'],
+        validate: {
+            validator: async function (v: Types.ObjectId) {
+                const room = await RoomModel.findById(v)
+                return room !== null && room !== undefined
+            },
+            message: 'No such room exists'
+        }
     },
     products: [{
         product: {
             type: Schema.Types.ObjectId,
             ref: 'Product',
-            required: [true, 'Product is required']
+            required: [true, 'Product is required'],
+            validate: {
+                validator: async function (v: Types.ObjectId) {
+                    const product = await ProductModel.findById(v)
+                    return product !== null && product !== undefined
+                },
+                message: 'No such product exists'
+            }
         },
         quantity: {
             type: Schema.Types.Number,
             required: [true, 'Quantity is required'],
+            min: [1, 'Quantity must be greater than 0'],
+            max: [100, 'Quantity must be less than or equal to 100'],
             default: 1
         },
         required: [true, 'Products are required']
@@ -51,11 +77,20 @@ const orderSchema = new Schema<IOrder>({
         option: {
             type: Schema.Types.ObjectId,
             ref: 'Option',
-            required: [true, 'Option is required']
+            required: [true, 'Option is required'],
+            validate: {
+                validator: async function (v: Types.ObjectId) {
+                    const option = await OptionModel.findById(v)
+                    return option !== null && option !== undefined
+                },
+                message: 'No such option exists'
+            }
         },
         quantity: {
             type: Schema.Types.Number,
             required: [true, 'Quantity is required'],
+            min: [1, 'Quantity must be greater than 0'],
+            max: [100, 'Quantity must be less than or equal to 100'],
             default: 1
         },
         required: false
