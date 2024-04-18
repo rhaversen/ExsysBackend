f1fedbebe46c61337d243fac9cef9cf986e35da6
rhaversen <rhaversen@gmail.com>
2024-04-18 21:44:03 +0200

    Setup boilerplate app (logging, error handling, database and config) Copied from personal project

---
+++ src/app/index.ts
@@ -0,0 +1,115 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import express from 'express'
+import mongoSanitize from 'express-mongo-sanitize'
+import helmet from 'helmet'
+import mongoose from 'mongoose'
+
+// Own Modules
+import databaseConnector from './utils/databaseConnector.js'
+import logger from './utils/logger.js'
+import config from './utils/setupConfig.js'
+
+// Routes
+import globalErrorHandler from './middleware/globalErrorHandler.js'
+
+// Logging environment
+logger.info(`Node environment: ${process.env.NODE_ENV}`)
+
+// Configs
+const {
+    expressPort
+} = config
+
+// Global variables and setup
+const app = express()
+
+// Connect to MongoDB in production and staging environment
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    await databaseConnector.connectToMongoDB()
+}
+
+// Middleware
+app.use(helmet())
+app.use(express.json())
+app.use(mongoSanitize())
+
+// Rate limiters
+// No routes yet
+
+// Use all routes with medium sensitivity rate limiter
+// no routes yet
+
+// Global error handler middleware
+app.use(globalErrorHandler)
+
+// Listen
+export const server = app.listen(expressPort, () => {
+    logger.info(`Express is listening at http://localhost:${expressPort}`)
+})
+
+// Handle unhandled rejections outside middleware
+process.on('unhandledRejection', (reason, promise): void => {
+    // Attempt to get a string representation of the promise
+    const promiseString = JSON.stringify(promise) !== '' ? JSON.stringify(promise) : 'a promise'
+
+    // Get a detailed string representation of the reason
+    const reasonDetail = reason instanceof Error ? reason.stack ?? reason.message : JSON.stringify(reason)
+
+    // Log the detailed error message
+    logger.error(`Unhandled Rejection at: ${promiseString}, reason: ${reasonDetail}`)
+
+    shutDown(1).catch(error => {
+        // If 'error' is an Error object, log its stack trace; otherwise, convert to string
+        const errorDetail = error instanceof Error ? error.stack ?? error.message : String(error)
+        logger.error(`An error occurred during shutdown: ${errorDetail}`)
+        process.exit(1)
+    })
+})
+
+// Handle uncaught exceptions outside middleware
+process.on('uncaughtException', (err): void => {
+    logger.error('Uncaught exception:', err)
+    shutDown(1).catch(error => {
+        logger.error('An error occurred during shutdown:', error)
+        process.exit(1)
+    })
+})
+
+// Assigning shutdown function to SIGINT signal
+process.on('SIGINT', (): void => {
+    logger.info('Received SIGINT')
+    shutDown().catch(error => {
+        logger.error('An error occurred during shutdown:', error)
+        process.exit(1)
+    })
+})
+
+// Assigning shutdown function to SIGTERM signal
+process.on('SIGTERM', (): void => {
+    logger.info('Received SIGTERM')
+    shutDown().catch(error => {
+        logger.error('An error occurred during shutdown:', error)
+        process.exit(1)
+    })
+})
+
+// Shutdown function
+export async function shutDown (exitCode?: number | undefined): Promise<void> {
+    try {
+        logger.info('Closing server...')
+        server.close()
+        logger.info('Server closed')
+        logger.info('Starting database disconnection...')
+        await mongoose.disconnect()
+        logger.info('Database disconnected')
+        logger.info('Shutdown completed')
+        process.exit(exitCode ?? 0)
+    } catch (error) {
+        logger.error('An error occurred during shutdown:', error)
+        process.exit(1) // Exit with code 1 indicating termination with error
+    }
+}
+
+export default app
---
+++ src/app/utils/logger.ts
@@ -0,0 +1,145 @@
+// Node.js built-in modules
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { Logtail } from '@logtail/node'
+
+// Third-party libraries
+import { createLogger, format as _format, transports as _transports } from 'winston'
+
+// Global variables
+const _filename = fileURLToPath(import.meta.url)
+const _dirname = dirname(_filename)
+const logDirectory = join(_dirname, (['production', 'staging'].includes(process.env.NODE_ENV ?? '') ? './logs/' : '../../logs/'))
+
+const winstonLogger = createLogger({
+    levels: {
+        error: 0,
+        warn: 1,
+        info: 2,
+        http: 3,
+        verbose: 4,
+        debug: 5,
+        silly: 6
+    },
+    format: _format.combine(
+        _format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:SSS' }),
+        _format.json(), // Use JSON format for logs
+        _format.printf((logObject) => {
+            return `${logObject.timestamp} ${logObject.level}: ${logObject.message}`
+        })
+    ),
+    defaultMeta: { service: 'exsys-backend' }, // Set a default metadata field
+    transports: [
+        new _transports.File({
+            filename: join(logDirectory, '../../logs/error.log'),
+            level: 'error'
+        }),
+        new _transports.File({
+            filename: join(logDirectory, '../../logs/info.log'),
+            level: 'info'
+        }),
+        new _transports.File({
+            filename: join(logDirectory, '../../logs/combined.log'),
+            level: 'silly'
+        }),
+        new _transports.Console({
+            format: _format.combine(
+                _format.colorize(),
+                _format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+                _format.printf((logObject) => {
+                    return `${logObject.timestamp} ${logObject.level}: ${logObject.message}`
+                })
+            ),
+            level: 'info'
+        })
+    ]
+})
+
+// Instantiate betterStackLogger only in production
+let betterStackLogger: Logtail | null = null
+
+function logToWinston (level: string, ...messages: any[]): void {
+    const combinedMessage = messages.join(' ')
+    switch (level) {
+        case 'error':
+            winstonLogger.error(combinedMessage)
+            break
+        case 'warn':
+            winstonLogger.warn(combinedMessage)
+            break
+        case 'info':
+            winstonLogger.info(combinedMessage)
+            break
+        case 'http':
+            winstonLogger.http(combinedMessage)
+            break
+        case 'verbose':
+            winstonLogger.verbose(combinedMessage)
+            break
+        case 'debug':
+            winstonLogger.debug(combinedMessage)
+            break
+        case 'silly':
+            winstonLogger.silly(combinedMessage)
+            break
+    }
+}
+
+async function logToBetterStack (level: string, ...messages: any[]): Promise<void> {
+    if (process.env.BETTERSTACK_LOG_TOKEN === null || process.env.BETTERSTACK_LOG_TOKEN === undefined || (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging')) {
+        return
+    }
+
+    if (betterStackLogger === null || betterStackLogger === undefined) {
+        betterStackLogger = new Logtail(process.env.BETTERSTACK_LOG_TOKEN)
+    }
+
+    const combinedMessage = messages.join(' ')
+    switch (level) {
+        case 'error':
+            await betterStackLogger.error(combinedMessage)
+            break
+        case 'warn':
+            await betterStackLogger.warn(combinedMessage)
+            break
+        case 'info':
+            await betterStackLogger.info(combinedMessage)
+            break
+        default:
+            await betterStackLogger.debug(combinedMessage)
+    }
+}
+
+function log (level: string, ...messages: unknown[]): void {
+    logToWinston(level, messages)
+    logToBetterStack(level, messages)
+        .catch((error) => {
+            logToWinston('error', `Error logging to BetterStack: ${error instanceof Error ? error.toString() : String(error)}`)
+        })
+}
+
+const logger = {
+    error: (...messages: unknown[]) => {
+        log('error', ...messages)
+    },
+    warn: (...messages: unknown[]) => {
+        log('warn', ...messages)
+    },
+    info: (...messages: unknown[]) => {
+        log('info', ...messages)
+    },
+    http: (...messages: unknown[]) => {
+        log('http', ...messages)
+    },
+    verbose: (...messages: unknown[]) => {
+        log('verbose', ...messages)
+    },
+    debug: (...messages: unknown[]) => {
+        log('debug', ...messages)
+    },
+    silly: (...messages: unknown[]) => {
+        log('silly', ...messages)
+    }
+}
+
+export default logger
