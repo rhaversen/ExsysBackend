4df7074cfe5c48a6e252b76b5f0ffef242f5fb69
rhaversen <rhaversen@gmail.com>
2024-04-18 23:12:58 +0200

    Add order model

---
+++ src/app/models/Order.ts
@@ -0,0 +1,84 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import { type Document, model, Schema, type Types } from 'mongoose'
+
+// Own modules
+import logger from '../utils/logger.js'
+
+// Destructuring and global variables
+
+// Interfaces
+export interface IOrder extends Document {
+    _id: Types.ObjectId
+    requestedDeliveryDate: Date // The date the order is supposed to be delivered
+    orderCreationDate: Date // The date the order was created in the system
+    roomName: Types.ObjectId // Reference to the Room document
+    products: Array<{
+        product: Types.ObjectId
+        quantity: number
+    }> // The products and their quantities
+    options?: Array<{
+        option: Types.ObjectId
+        quantity: number
+    }> // Additional options for the order
+}
+
+// Schema
+const orderSchema = new Schema<IOrder>({
+    requestedDeliveryDate: {
+        type: Schema.Types.Date,
+        required: [true, 'Requested delivery date is required']
+    },
+    orderCreationDate: {
+        type: Schema.Types.Date,
+        required: true,
+        default: Date.now
+    },
+    roomName: {
+        type: Schema.Types.ObjectId,
+        ref: 'Room',
+        required: [true, 'Room name is required']
+    },
+    products: [{
+        product: {
+            type: Schema.Types.ObjectId,
+            ref: 'Product',
+            required: [true, 'Product is required']
+        },
+        quantity: {
+            type: Schema.Types.Number,
+            required: [true, 'Quantity is required'],
+            default: 1
+        },
+        required: [true, 'Products are required']
+    }],
+    options: [{
+        option: {
+            type: Schema.Types.ObjectId,
+            ref: 'Option',
+            required: [true, 'Option is required']
+        },
+        quantity: {
+            type: Schema.Types.Number,
+            required: [true, 'Quantity is required'],
+            default: 1
+        },
+        required: false
+    }]
+})
+
+// Adding indexes
+orderSchema.index({ requestedDeliveryDate: -1 })
+
+// Pre-save middleware
+orderSchema.pre('save', function (next) {
+    logger.silly('Saving order')
+    next()
+})
+
+// Compile the schema into a model
+const OrderModel = model<IOrder>('Order', orderSchema)
+
+// Export the model
+export default OrderModel
