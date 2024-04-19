342185f4182869c13ceffef28ec4fbc79e6040f2
SatyDat <alexanderfrk@gmail.com>
2024-04-19 12:59:49 +0200

    filled out the product model for real this time

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -11,16 +11,79 @@ import logger from '../utils/logger.js'
 // Interfaces
 export interface IProduct extends Document {
     _id: Types.ObjectId
+    name: string
+    price: number
+    description: string
+    availability : number
+    orderWindow: {
+        from: {
+            hour: number
+            minute: number
+        }
+        to: {
+            hour: number
+            minute: number
+        }
+    }
+    options?: Array<{
+        option: Types.ObjectId
+        quantity: number
+    }>
+
 }
 
 // Schema
-const productSchema = new Schema<IProduct>({})
+const productSchema = new Schema<IProduct>({
+    name: {
+        type: Schema.Types.String,
+        required: [true, 'Product name is required']
+    },
+    price: {
+        type: Schema.Types.Number,
+        required: [true, 'Product price is required']
+    },
+    description: {
+        type: Schema.Types.String,
+        required: [true, 'Product description is required']
+    },
+    availability: {
+        type: Schema.Types.Number,
+        required: [true, 'Product availability is required'],
+        min: [0, 'Availability cannot be negative']
+    },
+    options: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Option'
+    }],
+    orderWindow: {
+        from: {
+            hour: {
+                type: Schema.Types.Number,
+                required: [true, 'Start hour is required']
+            },
+            minute: {
+                type: Schema.Types.Number,
+                required: [true, 'Start minute is required']
+            }
+        },
+        to: {
+            hour: {
+                type: Schema.Types.Number,
+                required: [true, 'End hour is required']
+            },
+            minute: {
+                type: Schema.Types.Number,
+                required: [true, 'End minute is required']
+            }
+        }
+    }
+})
 
 // Adding indexes
 
 // Pre-save middleware
 productSchema.pre('save', function (next) {
-    logger.silly('Saving order')
+    logger.silly('Saving product')
     next()
 })
 
