a15d3495401f0520e2b68714879085dc61848983
rhaversen <rhaversen@gmail.com>
2024-04-20 19:28:08 +0200

    Add admin model

---
+++ src/app/models/Admin.ts
@@ -0,0 +1,50 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import { type Document, model, Schema, type Types } from 'mongoose'
+import { hash } from 'bcrypt'
+
+// Own modules
+import logger from '../utils/logger.js'
+import config from '../utils/setupConfig.js'
+
+// Destructuring and global variables
+const {
+    bcryptSaltRounds
+} = config
+
+// Interfaces
+export interface IAdmin extends Document {
+    _id: Types.ObjectId
+    userName: string
+    email: string
+    password: string
+}
+
+// Schema
+const adminSchema = new Schema<IAdmin>({
+    userName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true, trim: true }
+}, {
+    timestamps: true
+})
+
+// Adding indexes
+
+// Pre-save middleware
+adminSchema.pre('save', async function (next) {
+    logger.silly('Saving admin')
+
+    // Password hashing
+    if (this.isModified('password')) {
+        this.password = await hash(this.password, bcryptSaltRounds) // Using a random salt for each user
+        next()
+    }
+})
+
+// Compile the schema into a model
+const AdminModel = model<IAdmin>('Room', adminSchema)
+
+// Export the model
+export default AdminModel
