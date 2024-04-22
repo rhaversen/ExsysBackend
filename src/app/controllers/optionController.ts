2d07654cc03c300ce91351bbed152eeab6dd6aea
rhaversen <rhaversen@gmail.com>
2024-04-22 02:05:07 +0200

    Linting (Use tabs)

--- src/app/controllers/optionController.ts
+++ src/app/controllers/optionController.ts
@@ -9,16 +9,16 @@ import OptionModel from '../models/Option'
 import logger from '../utils/logger'
 
 export async function createOption (req: Request, res: Response, next: NextFunction): Promise<void> {
-    logger.silly('Creating option')
+	logger.silly('Creating option')
 
-    try {
-        const newOption = await OptionModel.create(req.body as Record<string, unknown>)
-        res.status(201).json(newOption)
-    } catch (error) {
-        if (error instanceof mongoose.Error.ValidationError) {
-            res.status(400).json({ error: error.message })
-        } else {
-            next(error)
-        }
-    }
+	try {
+		const newOption = await OptionModel.create(req.body as Record<string, unknown>)
+		res.status(201).json(newOption)
+	} catch (error) {
+		if (error instanceof mongoose.Error.ValidationError) {
+			res.status(400).json({ error: error.message })
+		} else {
+			next(error)
+		}
+	}
 }
--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -20,39 +20,39 @@ let app: { shutDown: (exitCode?: number) => Promise<void>, server: Server }
 let chaiAppServer: ChaiHttp.Agent
 
 const cleanDatabase = async function (): Promise<void> {
-    /// ////////////////////////////////////////////
-    /// ///////////////////////////////////////////
-    if (process.env.NODE_ENV !== 'test') {
-        logger.error('Database wipe attempted in non-test environment! Shutting down.')
-        await app.shutDown(1)
-        return
-    }
-    /// ////////////////////////////////////////////
-    /// ///////////////////////////////////////////
-    logger.debug('Cleaning databases')
-    await mongoose.connection.db.dropDatabase()
+	/// ////////////////////////////////////////////
+	/// ///////////////////////////////////////////
+	if (process.env.NODE_ENV !== 'test') {
+		logger.error('Database wipe attempted in non-test environment! Shutting down.')
+		await app.shutDown(1)
+		return
+	}
+	/// ////////////////////////////////////////////
+	/// ///////////////////////////////////////////
+	logger.debug('Cleaning databases')
+	await mongoose.connection.db.dropDatabase()
 }
 
 before(async function () {
-    this.timeout(10000)
-    // Connect to the database
-    const database = await import('./mongoMemoryReplSetConnector.js')
-    await database.default()
+	this.timeout(10000)
+	// Connect to the database
+	const database = await import('./mongoMemoryReplSetConnector.js')
+	await database.default()
 
-    app = await import('../app/index.js')
+	app = await import('../app/index.js')
 })
 
 beforeEach(async function () {
-    chaiAppServer = chaiHttpObject.request(app.server).keepOpen()
+	chaiAppServer = chaiHttpObject.request(app.server).keepOpen()
 })
 
 afterEach(async function () {
-    sinon.restore()
-    await cleanDatabase()
+	sinon.restore()
+	await cleanDatabase()
 })
 
 after(function () {
-    void app.shutDown()
+	void app.shutDown()
 })
 
 export { chaiAppServer }
