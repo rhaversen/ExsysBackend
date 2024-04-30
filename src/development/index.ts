18e3a8f563950f2d825e670454c7c1aba11325ce
rhaversen <rhaversen@gmail.com>
2024-04-30 23:56:41 +0200

    Seed before importing app

--- src/development/index.ts
+++ src/development/index.ts
@@ -1,4 +1,4 @@
 import connectToMongoDB from '../test/mongoMemoryReplSetConnector.js'
 await connectToMongoDB()
-await import('../app/index.js')
 await import('./seedDatabase.js')
+await import('../app/index.js')
