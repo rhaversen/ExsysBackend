531b993bef895a85cdc1abd859fbbca36aff60da
rhaversen <rhaversen@gmail.com>
2024-05-04 14:23:46 +0200

    Update seeds

--- src/development/seedDatabase.ts
+++ src/development/seedDatabase.ts
@@ -5,178 +5,258 @@ import logger from '../app/utils/logger.js'
 logger.info('Seeding database')
 
 await ProductModel.create({
-	name: 'Burger',
-	price: 50,
-	description: 'A delicious burger all day',
+	name: '0 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 0,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 0, minute: 0 },
+		to: { hour: 1, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Pizza',
-	price: 70,
-	description: 'A delicious pizza all day',
+	name: '1 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 0,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 1, minute: 0 },
+		to: { hour: 2, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Pasta',
-	price: 60,
-	description: 'A delicious pasta all day',
+	name: '2 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 0,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 2, minute: 0 },
+		to: { hour: 3, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Coffee',
-	price: 100,
-	description: 'A delicious morning coffee',
+	name: '3 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 0,
-			minute: 0
-		},
-		to: {
-			hour: 12,
-			minute: 0
-		}
-	}
+		from: { hour: 3, minute: 0 },
+		to: { hour: 4, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Juice',
-	price: 40,
-	description: 'A delicious morning juice',
+	name: '4 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 0,
-			minute: 0
-		},
-		to: {
-			hour: 12,
-			minute: 0
-		}
-	}
+		from: { hour: 4, minute: 0 },
+		to: { hour: 5, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Sandwich',
-	price: 30,
-	description: 'A delicious morning sandwich',
+	name: '5 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 0,
-			minute: 0
-		},
-		to: {
-			hour: 12,
-			minute: 0
-		}
-	}
+		from: { hour: 5, minute: 0 },
+		to: { hour: 6, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Cake',
-	price: 30,
-	description: 'A delicious afternoon cake',
+	name: '6 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 12,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 6, minute: 0 },
+		to: { hour: 7, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Tea',
-	price: 20,
-	description: 'A delicious evening tea',
+	name: '7 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 12,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 7, minute: 0 },
+		to: { hour: 8, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Beer',
-	price: 60,
-	description: 'A delicious evening beer',
+	name: '8 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 18,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 8, minute: 0 },
+		to: { hour: 9, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await ProductModel.create({
-	name: 'Wine',
-	price: 80,
-	description: 'A delicious evening wine',
+	name: '9 Cake',
+	price: 2,
 	orderWindow: {
-		from: {
-			hour: 18,
-			minute: 0
-		},
-		to: {
-			hour: 23,
-			minute: 59
-		}
-	}
+		from: { hour: 9, minute: 0 },
+		to: { hour: 10, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '10 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 10, minute: 0 },
+		to: { hour: 11, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '11 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 11, minute: 0 },
+		to: { hour: 12, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '12 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 12, minute: 0 },
+		to: { hour: 13, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '13 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 13, minute: 0 },
+		to: { hour: 14, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '14 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 14, minute: 0 },
+		to: { hour: 15, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '15 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 15, minute: 0 },
+		to: { hour: 16, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '16 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 16, minute: 0 },
+		to: { hour: 17, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '17 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 17, minute: 0 },
+		to: { hour: 18, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '18 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 18, minute: 0 },
+		to: { hour: 19, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '19 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 19, minute: 0 },
+		to: { hour: 20, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '20 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 20, minute: 0 },
+		to: { hour: 21, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '21 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 21, minute: 0 },
+		to: { hour: 22, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '22 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 22, minute: 0 },
+		to: { hour: 23, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
+})
+
+await ProductModel.create({
+	name: '23 Cake',
+	price: 2,
+	orderWindow: {
+		from: { hour: 23, minute: 0 },
+		to: { hour: 0, minute: 0 }
+	},
+	imageURL: 'https://via.placeholder.com/150'
 })
 
 await RoomModel.create({
 	name: 'Billiard Room',
-	description: 'A room'
+	description: 'A room for billiards'
 })
 
 await RoomModel.create({
 	name: 'Library Room',
-	description: 'Another room'
+	description: 'A room for reading'
 })
 
 await RoomModel.create({
 	name: 'Conservatory Room',
-	description: 'Yet another room'
+	description: 'A room for music'
 })
 
 await RoomModel.create({
