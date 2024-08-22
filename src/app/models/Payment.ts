2baf005cd4baec74b7d9520d476916ebe68b79ff
Rasmus Haversen <rhaversen@gmail.com>
2024-08-23 01:41:54 +0200

    Add Payment model

---
+++ src/app/models/Payment.ts
@@ -0,0 +1,53 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import { type Document, model, Schema } from 'mongoose'
+
+// Own modules
+import logger from '../utils/logger.js'
+
+// Interfaces
+export interface IPayment extends Document {
+	// Properties
+	_id: Schema.Types.ObjectId
+	clientTransactionId: string
+	paymentStatus: 'pending' | 'successful' | 'failed'
+
+	// Timestamps
+	createdAt: Date
+	updatedAt: Date
+
+	// Methods
+}
+
+// Schema
+const paymentSchema = new Schema<IPayment>({
+	clientTransactionId: {
+		type: String,
+		required: [true, 'Client transaction ID is required'],
+		unique: true
+	},
+	paymentStatus: {
+		type: String,
+		enum: ['pending', 'successful', 'failed'],
+		default: 'pending'
+	}
+}, {
+	timestamps: true
+})
+
+// Validations
+
+// Pre-save middleware
+paymentSchema.pre('save', async function (next) {
+	logger.silly('Saving kiosk')
+	next()
+})
+
+// Payment methods
+
+// Compile the schema into a model
+const PaymentModel = model<IPayment>('Payment', paymentSchema)
+
+// Export the model
+export default PaymentModel
